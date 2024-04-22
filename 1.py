be131d34eee35564f341f6685e301a358c46b1de
Ekaterina <mkcsscjbsvc@,fds.ccc>
2024-04-22 12:53:25 +0400

    #hi

--- 1.py
+++ 1.py
@@ -1,20 +1,16 @@
-import sqlite3
+import csv
+import json
 
-con = sqlite3.connect("music_db.sqlite")
-
-cur = con.cursor()
-
-result = cur.execute("""SELECT DISTINCT
-    Track.Name,
-    Artist.Name
-FROM
-    Track
-LEFT JOIN Album ON Track.AlbumId = Album.AlbumId
-LEFT JOIN Artist ON Album.ArtistId = Artist.ArtistId
-WHERE Artist.Name=?
-ORDER BY Track.Name;""", (input(),)).fetchall()
-t = []
-for i, x in result:
-    print(i)
-
-con.close()
+with open("danger.csv") as in_file:
+    reader = csv.DictReader(in_file, delimiter=";")
+    w = {}
+    for line in reader:
+        w.setdefault(line['monster'], [])
+        w[line['monster']].append(line['danger'])
+for i in w:
+    l = w[i]
+    l.sort(reverse=True)
+    w[i] = l
+h = dict(sorted(w.items(), key=lambda x: x[0], reverse=True))
+with open('monsters.json', "w") as out_file:
+    json.dump(h, out_file)
