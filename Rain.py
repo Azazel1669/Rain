18ad9c811619999bb93a3f1f8974a79633fc2c18
Ekaterina <mkcsscjbsvc@,fds.ccc>
2024-04-22 13:44:42 +0400

    I want it painted black

--- Rain.py
+++ Rain.py
@@ -58,58 +58,58 @@ def enter():
     return render_template("enter.html")
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.name == form.name.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            password=form.password.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.name == form.name.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-def main():
-    db_session.global_init(f"db/{DB_NAME}.db")
-    #user = User()
-    #user.name = "Пользователь 2"
-    #user.password = "qwerty123"
-    #user.about = 'hi everyone'
-    #db_sess = db_session.create_session()
-    #db_sess.add(user)
-    #db_sess.commit()
-    #fill_users(DB_NAME)
-    app.register_blueprint(fan_api.blueprint)
-    app.run(port=8080, host='127.0.0.1')
+# @app.route('/register', methods=['GET', 'POST'])
+# def reqister():
+#     form = RegisterForm()
+#     if form.validate_on_submit():
+#         if form.password.data != form.password_again.data:
+#             return render_template('register.html', title='Регистрация',
+#                                    form=form,
+#                                    message="Пароли не совпадают")
+#         db_sess = db_session.create_session()
+#         if db_sess.query(User).filter(User.name == form.name.data).first():
+#             return render_template('register.html', title='Регистрация',
+#                                    form=form,
+#                                    message="Такой пользователь уже есть")
+#         user = User(
+#             name=form.name.data,
+#             password=form.password.data,
+#             about=form.about.data
+#         )
+#         user.set_password(form.password.data)
+#         db_sess.add(user)
+#         db_sess.commit()
+#         return redirect('/login')
+#     return render_template('register.html', title='Регистрация', form=form)
+#
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         db_sess = db_session.create_session()
+#         user = db_sess.query(User).filter(User.name == form.name.data).first()
+#         if user and user.check_password(form.password.data):
+#             login_user(user, remember=form.remember_me.data)
+#             return redirect("/")
+#         return render_template('login.html',
+#                                message="Неправильный логин или пароль",
+#                                form=form)
+#     return render_template('login.html', title='Авторизация', form=form)
+#
+#
+# def main():
+#     db_session.global_init(f"db/{DB_NAME}.db")
+#     #user = User()
+#     #user.name = "Пользователь 2"
+#     #user.password = "qwerty123"
+#     #user.about = 'hi everyone'
+#     #db_sess = db_session.create_session()
+#     #db_sess.add(user)
+#     #db_sess.commit()
+#     #fill_users(DB_NAME)
+#     app.register_blueprint(fan_api.blueprint)
+#     app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
